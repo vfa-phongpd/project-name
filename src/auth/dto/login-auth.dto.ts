30af7c4d3ca4cbfc34d3b87281bbc29696237fdf
vfa-phongpd <vfa.internship.phongpd@gmail.com>
2023-08-14 08:42:30 +0700

    update

--- src/auth/dto/login-auth.dto.ts
+++ src/auth/dto/login-auth.dto.ts
@@ -11,9 +11,9 @@ export class LoginUserAuthDto {
     @IsString()
     @MinLength(6)
     @MaxLength(20)
-    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,20}$/, {
-        message: 'Password too weak. It must contain at least one lowercase letter, one uppercase letter, one number, and one special character.',
-    })
+    // @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,20}$/, {
+    //     message: 'Password too weak. It must contain at least one lowercase letter, one uppercase letter, one number, and one special character.',
+    // })
     password: string;
 
 }
